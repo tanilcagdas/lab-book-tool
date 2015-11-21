4acc6488112e2827d64334b50ee9d2aadf90d980
tanil <tanil@tanilspc>
2015-11-21 23:21:42 +0200

    new module jpa-repository pom name changes

--- lab-book-tool/server/src/main/java/com/labbooktool/repository/LabConstants.java
+++
@@ -1,8 +0,0 @@
-package com.labbooktool.repository;
-
-public class LabConstants {
-	public static final String DEVICES_TABLE = "DEVICES";
-	public static final String LAPTOPS_TABLE = "LAPTOPS";
-	public static final String AVAILABLE = "available";
-
-}
--- lab-book-tool/server/src/main/java/com/labbooktool/server/LaptopDaoIf.java
+++
@@ -1,7 +0,0 @@
-package com.labbooktool.server;
-
-import com.labbooktool.repository.ItemRepository;
-
-public interface LaptopDaoIf extends ItemRepository{
-
-}
