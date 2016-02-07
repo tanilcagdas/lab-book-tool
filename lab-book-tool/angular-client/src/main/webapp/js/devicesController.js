7d8913505739343caecce795298531bcc2d4cb3f
tanilcagdas <tanilcagdas@192.168.0.18>
2016-02-07 02:17:28 +0200

    angular client

---
+++ lab-book-tool/angular-client/src/main/webapp/js/devicesController.js
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+
+app.controller('devicesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+});
---
+++ lab-book-tool/angular-client/src/main/webapp/js/main.js
@@ -0,0 +1,97 @@
+/**
+ * AngularJS Tutorial 1
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ */
+
+/**
+ * Main AngularJS Web Application
+ */
+var app = angular.module('tutorialWebApp', [
+  'ngRoute'
+]);
+
+/**
+ * Configure the Routes
+ */
+app.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider
+    // Home
+    .when("/", {templateUrl: "partials/devices.html", controller: "PageCtrl"})
+    // Pages
+    .when("/devices", {templateUrl: "partials/devices.html", controller: "PageCtrl"})
+    .when("/laptops", {templateUrl: "partials/laptops.html", controller: "PageCtrl"})
+ 
+    // Blog
+
+    // else 404
+    .otherwise("/404", {templateUrl: "partials/404.html", controller: "PageCtrl"});
+}]);
+
+/**
+ * Controls the Blog
+ */
+app.controller('BlogCtrl', function (/* $scope, $location, $http */) {
+  console.log("Blog Controller reporting for duty.");
+});
+
+/**
+ * Controls all other Pages
+ */
+app.controller('PageCtrl', function (/* $scope, $location, $http */) {
+  console.log("Page Controller reporting for duty.");
+
+  // Activates the Carousel
+  $('.carousel').carousel({
+    interval: 5000
+  });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
+
+
+app.controller('devicesCtrl', function($scope, $http) {
+	var req =
+	{ 
+			method: 'GET',
+			url: 'http://localhost:8080/rest/webresources/labBook/device',
+			headers: {
+			   'Content-Type': undefined,
+			   'username' : 'admin'
+			 },
+			 data: { test: 'test' }
+			}
+//	$http.get("http://localhost:8080/rest/webresources/labBook/device").
+	$http(req).then(function(response) {
+		$scope.devices = response.data;
+		console.log($scope.devices)
+		
+	}, function errorCallback(response) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+});
+
+app.controller('laptopCtrl', function($scope, $http) {
+	var req =
+	{ 
+			method: 'GET',
+			url: 'http://localhost:8080/rest/webresources/labBook/laptop',
+			headers: {
+			   'Content-Type': undefined,
+			   'username' : 'admin'
+			 },
+			 data: { test: 'test' }
+			}
+//	$http.get("http://localhost:8080/rest/webresources/labBook/device").
+	$http(req).then(function(response) {
+		$scope.laptops = response.data;
+		console.log($scope.laptops)
+		
+	}, function errorCallback(response) {
+	    // called asynchronously if an error occurs
+	    // or server returns response with an error status.
+	  });
+});
